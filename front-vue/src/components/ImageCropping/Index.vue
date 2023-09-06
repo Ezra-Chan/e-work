010df42dfd943b43cd1ab883bbd0b9943b467d4a
ezra <26107204+Ezra-Chan@users.noreply.github.com>
2023-09-06 23:02:14 +0800

    feat: temp save image cropping
    Change-Id: Id0154fb21e5246455b222876fde8297aef9f5cf0

---
+++ front-vue/src/components/ImageCropping/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="image-cropping"></div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'ImageCropping',
+})
+</script>
+
+<style lang="less" scoped></style>
---
+++ front-vue/src/components/ImageCropping/Modal.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="image-cropping-modal"></div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'ImageCroppingModal',
+});
+</script>
+
+<style lang="less" scoped></style>
