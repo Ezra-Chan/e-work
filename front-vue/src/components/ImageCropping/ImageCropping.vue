501e05c0f1614fc97c8f9be37b928f890be98676
ezra <26107204+Ezra-Chan@users.noreply.github.com>
2023-09-16 23:55:22 +0800

    feat: temp save image cropping
    Change-Id: I2ef649735b8dbf2fffd32c55649d8145f949f22f

--- front-vue/src/components/ImageCropping/ImageCropping.vue
+++ front-vue/src/components/ImageCropping/ImageCropping.vue
@@ -2,6 +2,11 @@
   <div class="image-cropping">图片裁切</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits<{
+  upload: [data: any];
+  close: [];
+}>();
+</script>
 
 <style lang="less" scoped></style>
