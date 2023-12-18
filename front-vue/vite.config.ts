0b1efd7c38d09c87dac905ba805b72d678cdd726
ezra <26107204+Ezra-Chan@users.noreply.github.com>
2023-12-18 11:43:46 +0800

    chore: add vite-plugin-vue-devtools
    Change-Id: I7393d091f58479616e9a085bd544868b1c04c039

--- front-vue/vite.config.ts
+++ front-vue/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import type { ConfigEnv } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 import ReactivityTransform from '@vue-macros/reactivity-transform/dist/vite';
 import Vue from '@vitejs/plugin-vue';
 // import VueJsx from '@vitejs/plugin-vue-jsx'
@@ -28,6 +29,7 @@ export default defineConfig(({ mode }: ConfigEnv) => {
       extensions: ['.js', '.json', '.ts', '.vue'],
     },
     plugins: [
+      VueDevTools(),
       Vue(),
       DefineOptions(),
       ReactivityTransform(),
